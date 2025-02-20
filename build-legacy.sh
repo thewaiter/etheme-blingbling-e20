a381aeab8581900a57cf2a37424f40ddd4fb08f5
Robert Wiley <ylee@bodhilinux.com>
2025-02-20 03:24:45 -0500

    Add support for BL7

---
+++ build-legacy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright 2025 Bodhi Linux
+
+printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
+cd etheme-blingbling-e20
+sed -i 's/MokshaBlingBling/Icons-Moksha-BlingBling/g' blingbling.edc
+./build.sh
+sed -i 's/Icons-Moksha-BlingBling/MokshaBlingBling/g' blingbling.edc
+cd ..
