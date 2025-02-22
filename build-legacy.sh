fa092ca32a902688ae5f8ad8f5745f706009159e
Robert Wiley <ylee@bodhilinux.com>
2025-02-22 12:24:47 -0500

    Normalize gtk theme name

--- build-legacy.sh
+++ build-legacy.sh
@@ -4,6 +4,8 @@
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd etheme-blingbling-e20
 sed -i 's/MokshaBlingBling/Icons-Moksha-BlingBling/g' blingbling.edc
+sed -i 's/MokshaBlingBling/Bling-GTK/g' blingbling.edc
 ./build.sh
 sed -i 's/Icons-Moksha-BlingBling/MokshaBlingBling/g' blingbling.edc
+sed -i 's/Bling-GTK/MokshaBlingBling/g' blingbling.edc
 cd ..
