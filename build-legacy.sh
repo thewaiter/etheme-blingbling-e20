360947671af81294dcf9321aee8e1a1a75431051
Robert Wiley <ylee@bodhilinux.com>
2025-05-18 08:50:08 -0400

    Improve legacy build script

--- build-legacy.sh
+++ build-legacy.sh
@@ -3,9 +3,11 @@
 
 printf "\n\033[1;33mWARNING:\033[0m For use with Bodhi Linux 7 only.\n\n"
 cd etheme-blingbling-e20
-sed -i 's/MokshaBlingBling/Icons-Moksha-BlingBling/g' blingbling.edc
-sed -i 's/MokshaBlingBling/Bling-GTK/g' blingbling.edc
+sed -i \
+    -e 's/item: "gtk-theme" *"[^"]*"/item: "gtk-theme"     "Bling-GTK"/' \
+    -e 's/item: "icon-theme" *"[^"]*"/item: "icon-theme"    "Icons-Moksha-BlingBling"/' \
+    blingbling.edc
 ./build.sh
-sed -i 's/Icons-Moksha-BlingBling/MokshaBlingBling/g' blingbling.edc
-sed -i 's/Bling-GTK/MokshaBlingBling/g' blingbling.edc
+git restore blingbling.edc
 cd ..
+
